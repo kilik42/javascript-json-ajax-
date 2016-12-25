4e4bca06a215a3686ee127237f89237805065085
marvin evins <maevins@cps.edu>
2016-12-25 04:34:25 -0600

    jsonbranch
    json branch with multiple urls

---
+++ projectTwo/js/ajax.js
@@ -0,0 +1,27 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+        var ourRequest = new XMLHttpRequest();
+
+        ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1' + pageCounter + '.json');
+
+        ourRequest.onLoad = function(){
+            var ourData = JSON.parse(OurRequest.responseText);
+
+            //console.log(ourData[0]);
+            renderHTML(ourData);
+        }
+
+        ourRequest.send();
+        pageCounter++;
+});
+
+function renderHTML(data) {
+    var htmlString = ' ';
+    for (i = 0; i < data.length; i++){
+        htmlString += "<p> " + data[i].name +  "is a " + data[i].species + "</p>";
+    }
+       animalContainer.insertAdjacentHTM('beforeend', htmlString ); 
+}
