21b4d371751793077229427130c351acd79d8913
marvin evins <maevins@cps.edu>
2016-12-25 05:40:22 -0600

    addederrorhandling
    added error handling

--- projectTwo/js/ajax.js
+++ projectTwo/js/ajax.js
@@ -8,20 +8,53 @@ btn.addEventListener("click", function(){
         ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1' + pageCounter + '.json');
 
         ourRequest.onLoad = function(){
-            var ourData = JSON.parse(OurRequest.responseText);
+            if(ourRequest.status >= 200 && ourRequest.status < 400){
+                    var ourData = JSON.parse(OurRequest.responseText);
 
             //console.log(ourData[0]);
-            renderHTML(ourData);
+                    renderHTML(ourData);
+            } else {
+                console.llg('we connected to the server, but it returned an error');
+            }
+            
         }
 
+        ourRequest.onerror = function(){
+                console.log("connection error");
+
+        };
         ourRequest.send();
         pageCounter++;
+        if (pageCounter > 3){
+            btn.classList.add("hideme");
+        }
 });
 
 function renderHTML(data) {
     var htmlString = ' ';
     for (i = 0; i < data.length; i++){
-        htmlString += "<p> " + data[i].name +  "is a " + data[i].species + "</p>";
+        htmlString += "<p> " + data[i].name +  "is a " + data[i].species + "that likes to eat ";
+        for( ii = 0; ii <data[i].foods.likes.length; ii++){
+            if(ii ==0){
+                htmlSting += data[i].foods.likes[ii];
+            } else {
+                htmlString += " and " + data[i].foods.likes[ii];
+            }
+
+        }
+        htmString += 'and dislikes';
+
+        
+        for( ii = 0; ii <data[i].foods.dislikes.length; ii++){
+            if(ii ==0){
+                htmlSting += data[i].foods.dislikes[ii];
+            } else {
+                htmlString += " and " + data[i].foods.disikes[ii];
+            }
+
+        }
+        htmlString += '.</p>';
+
     }
        animalContainer.insertAdjacentHTM('beforeend', htmlString ); 
 }
